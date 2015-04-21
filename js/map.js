519f0a4f7cf8de73858f966c188b586a35b345bd
Shuoqing Ding <dsq704136@gmail.com>
2015-04-20 21:49:44 -0700

    add location search

--- js/map.js
+++ js/map.js
@@ -3,6 +3,25 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 var map;
 
+function search( loc ){
+  if( loc == "SGM" ){
+    console.log( loc );
+    setMarkers( [ [34.021449, -118.289094, loc] ] );
+  }
+  else if( loc == "THH" ){
+    setMarkers( [ [34.022272, -118.284400,loc] ] );
+  }
+  else if( loc == "SAL" ){
+    setMarkers( [ [34.019453, -118.289480,loc] ] );
+  }
+  else if( loc == "OHE" ){
+    setMarkers( [ [34.020718, -118.289480,loc] ] );
+  }
+  else{
+    $.simplyToast( loc + ' not found!', 'danger');
+  }
+}
+
 function initialize() {
 
   /* position Amsterdam */
@@ -23,6 +42,11 @@ function initialize() {
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   // marker.setMap(map);
  
+var loc = GetParameterValues( "loc" );
+$("#loc_search").val( loc );
+search( loc );
+
+
 };
 
 var mks = [];
@@ -33,6 +57,16 @@ var parking = [ [34.0202158,-118.2912245], [34.0216649,-118.289636], [34.0231188
 var transport = [ [34.0240052,-118.2870032], [34.0224706,-118.2833248] ];
 var lib = [ [34.0236214,-118.2868131], [34.0221442,-118.284261], [34.0210612,-118.2844513], [34.0208745,-118.2854598] ];
 var hospital = [ [34.0240067,-118.2886758] ];
+
+function GetParameterValues(param) {  
+            var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');  
+            for (var i = 0; i < url.length; i++) {  
+                var urlparam = url[i].split('=');  
+                if (urlparam[0] == param) {  
+                    return urlparam[1];  
+                }  
+            }  
+        }  
 
 
 function setMarkers( markers ){
@@ -79,6 +113,11 @@ $("#park_icon").click( function(){
 $("#gym_icon").click( function(){
   setMarkers(gym);
 } );
+
+$("#button_search").click( function(){
+  search( $("#loc_search").val() );
+
+});
 
 
 /* end google maps -----------------------------------------------------*/
